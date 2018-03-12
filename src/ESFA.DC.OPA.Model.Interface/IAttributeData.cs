a9060ccf1a6884d65eb9c395d8d99acacac6ba92
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-03-12 12:55:49 +0000

    40352 - Adding OPA Model Interface

---
+++ src/ESFA.DC.OPA.Model.Interface/IAttributeData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.OPA.Model.Interface
+{
+    public interface IAttributeData
+    {
+        string Name { get; }
+        IList<ITemporalValueItem> Changepoints { get; }
+        object Value { get; }
+        bool IsTemporal { get; }
+        void AddChangepoint(ITemporalValueItem temporalValue);
+        void AddChangepoints(IEnumerable<ITemporalValueItem> temporalValues);
+    }
+}
+
---
+++ src/ESFA.DC.OPA.Model.Interface/ITemporalValueItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ESFA.DC.OPA.Model.Interface
+{
+    public interface ITemporalValueItem
+    {
+        DateTime ChangePoint { get; }
+        object Value { get; }
+        string Type { get; }
+    }
+}
